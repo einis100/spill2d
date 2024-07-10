291b352e78d9a0d083c33ea75d3dcc27966ae3be
trygvea <trygve.amundsen@gmail.com>
2024-07-10 15:29:27 +0200

    Made background resizable

--- RisUtils.js
+++ RisUtils.js
@@ -34,4 +34,41 @@ function rectangularCollision({rectangle1, rectangle2}) {
     if (timer === 0) {
         determineWinner({player, enemy, timerId})
     }
- }  
+ }
+
+function loadBackgroundImage(imageSrc) {
+    const image = new Image()
+    image.src = imageSrc
+    return image
+}
+
+ function drawBackground(c, image, xZoom, yZoom) {
+    if (!image) return
+    c.drawImage(
+        image,
+        0,
+        0,
+        window.innerWidth,
+        window.innerHeight + 145*yZoom // There is a gap at the bottom of the image we don't want to show
+    )
+}
+
+// Game was originally made for resolution 1366 x 768 which will use a zoom of 1
+const ORIGINAL_WIDTH = 1366;
+const ORIGINAL_HEIGHT = 768;
+
+let xZoom = 1;
+let yZoom = 1;
+
+function setZoom() {
+    xZoom = window.innerWidth / ORIGINAL_WIDTH;
+    yZoom = window.innerHeight / ORIGINAL_HEIGHT;
+}
+window.addEventListener('resize', () => {
+    setZoom();
+    animate()
+});
+
+window.addEventListener('load', () => {
+    setZoom();
+});
